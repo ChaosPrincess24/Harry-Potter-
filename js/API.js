f3f820bc332d55b05609c389a0d68d3f46a48751
ChaosPrincess24 <54142664+ChaosPrincess24@users.noreply.github.com>
2020-11-10 12:56:36 +0200

    Sorting hat

---
+++ js/API.js
@@ -0,0 +1,45 @@
+var apiUrl = 'https://www.potterapi.com/v1';
+
+var characterPath = '/characters';
+var sortingHatPath = '/sortingHat';
+var housesPath = '/houses';
+var spellsPath = '/spells';
+
+var apiKey = '$2a$10$jBnqiABzFBpt6e1Pc0y3POmMcwde52laEMC4GGuMPSpdXiA4l5GGC'
+
+var queryString = '?key=' + apiKey;
+
+var resourceUrl = apiUrl + characterPath + queryString;
+
+async function getCharacters() {
+    var response = await fetch(resourceUrl);
+    var data = await response.json();
+    if (response.ok) {
+        formatCharacters(data);
+        return data;
+    }
+}
+
+function formatCharacters(characters) {
+    var chars = document.createElement('ul');
+    characters.forEach(function(character) {
+        var charContainer = document.createElement('li');
+        var charName = document.createElement('span');
+        charName.innerText = character.name;
+        charContainer.appendChild(charName);
+        if (character.house) {
+            var charHouse = document.createElement('div');
+            charHouse.classList.add('house');
+            charHouse.innerText = character.house;
+            charContainer.appendChild(charHouse);
+        }
+        chars.appendChild(charContainer);
+    });
+    document.body.appendChild(chars);
+}
+
+function sortingHat(houses) {
+    var house = data.apiUrl.sortingHatPath;
+
+    $(".sortinghat").append(houses);
+}
